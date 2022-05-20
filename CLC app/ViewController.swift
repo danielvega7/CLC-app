59e6b31c3cc5a408a7a839510b6da482672b856a
Daniel Vega <drvega7@icloud.com>
2022-05-20 06:12:44 -0500

    Initial Commit

---
+++ CLC app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLC app
+//
+//  Created by Daniel Vega on 5/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
