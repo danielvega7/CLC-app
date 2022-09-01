97bf14e8d7355f96bc7ddfe08cfa5611af7214bb
danielvega7 <daniel.vega23@d155.org>
2022-09-01 09:51:42 -0500

    started auth

--- CLC app/ViewController.swift
+++ CLC app/ViewController.swift
@@ -6,13 +6,33 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class ViewController: UIViewController {
-
+    var actionCodeSettings = ActionCodeSettings()
+    
+   
+   
+    
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        actionCodeSettings.url = URL(string: "https://example.appspot.com")
+        actionCodeSettings.handleCodeInApp = true
+        actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+
+        let provider = FUIEmailAuth(authUI: FUIAuth.defaultAuthUI()!,
+                                    signInMethod: FIREmailLinkAuthSignInMethod,
+                                    forceSameDevice: false,
+                                    allowNewEmailAccounts: true,
+                                    actionCodeSetting: actionCodeSettings)
+        
     }
+    
 
 
 }
