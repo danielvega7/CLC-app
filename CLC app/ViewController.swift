e0d0cbaa06e9962d2b488c0f14a442f9cdfd7020
danielvega7 <daniel.vega23@d155.org>
2022-09-02 08:10:57 -0500

    more work on authorization, domain error going on rn

--- CLC app/ViewController.swift
+++ CLC app/ViewController.swift
@@ -7,12 +7,12 @@
 
 import UIKit
 import FirebaseAuth
-import Firebase
 
 class ViewController: UIViewController {
     var actionCodeSettings = ActionCodeSettings()
     
-   
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
    
     
    
@@ -21,19 +21,55 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        actionCodeSettings.url = URL(string: "https://example.appspot.com")
-        actionCodeSettings.handleCodeInApp = true
-        actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+        
 
-        let provider = FUIEmailAuth(authUI: FUIAuth.defaultAuthUI()!,
-                                    signInMethod: FIREmailLinkAuthSignInMethod,
-                                    forceSameDevice: false,
-                                    allowNewEmailAccounts: true,
-                                    actionCodeSetting: actionCodeSettings)
+//        let provider = FUIEmailAuth(authUI: FUIAuth.defaultAuthUI()!,signInMethod: FIREmailLinkAuthSignInMethod,forceSameDevice: false,allowNewEmailAccounts: true,actionCodeSetting: actionCodeSettings)
         
     }
     
-
+    @IBAction func clickButtonAction(_ sender: UIButton) {
+        if let email = textFieldOutlet.text {
+             
+               // [START action_code_settings]
+               let actionCodeSettings = ActionCodeSettings()
+               actionCodeSettings.url = URL(string: "https://www.google.com/")
+               // The sign-in operation has to always be completed in the app.
+               actionCodeSettings.handleCodeInApp = true
+               actionCodeSettings.setIOSBundleID(Bundle.main.bundleIdentifier!)
+              
+               // [END action_code_settings]
+               // [START send_signin_link]
+               Auth.auth().sendSignInLink(toEmail: email,
+                                          actionCodeSettings: actionCodeSettings) { error in
+                 // [START_EXCLUDE]
+                 
+                   // [END_EXCLUDE]
+                   if let error = error {
+                     print(error)
+                     print(error.localizedDescription + " it didn't work pendejo")
+                     return
+                   }
+                   // The link was successfully sent. Inform the user.
+                   // Save the email locally so you don't need to ask the user for it again
+                   // if they open the link on the same device.
+                   UserDefaults.standard.set(email, forKey: "Email")
+                   print("Check your email for link")
+                   // [START_EXCLUDE]
+                 
+                 // [END_EXCLUDE]
+               
+               // [END send_signin_link]
+             }
+           } else {
+             print("Email can't be empty")
+           }
+        
+        
+       
+        
+        
+    }
+    
 
 }
 
